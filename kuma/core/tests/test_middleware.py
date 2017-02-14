06b316f72c927a81d0b1d742d5656017030b95d9
rjohnson <rjohnson@mozilla.com>
2017-02-13 16:57:42 -0800

    make session middleware aware of maintenance mode
    * remove NoVarySessionMiddleware (it's confusing as
      it's effect is cancelled by Django's SessionMiddleware
      further out in the middleware onion)

--- kuma/core/tests/test_middleware.py
+++ kuma/core/tests/test_middleware.py
@@ -1,7 +1,13 @@
+import pytest
+from mock import MagicMock
+
 from django.test import RequestFactory
 
 from kuma.core.tests import KumaTestCase, eq_
-from ..middleware import SetRemoteAddrFromForwardedFor
+from kuma.core.middleware import (
+    MMAwareSessionMiddleware,
+    SetRemoteAddrFromForwardedFor
+)
 
 
 class TrailingSlashMiddlewareTestCase(KumaTestCase):
@@ -36,3 +42,26 @@ class SetRemoteAddrFromForwardedForTestCase(KumaTestCase):
         req3 = rf.get('/', HTTP_X_FORWARDED_FOR='3.3.3.3, 4.4.4.4')
         middleware.process_request(req3)
         eq_(req3.META['REMOTE_ADDR'], '3.3.3.3')
+
+
+@pytest.mark.parametrize("maintenance_mode", [True, False])
+def test_maintenance_mode_aware_session_middleware(rf, settings,
+                                                   maintenance_mode):
+    request = rf.get('/foo')
+    request.COOKIES[settings.SESSION_COOKIE_NAME] = 'totallyfake'
+
+    settings.MAINTENANCE_MODE = maintenance_mode
+
+    middleware = MMAwareSessionMiddleware()
+    middleware.process_request(request)
+
+    if maintenance_mode:
+        assert request.session
+        assert request.session.session_key is None
+
+        response = middleware.process_response(request, MagicMock())
+
+        assert not response.method_calls
+    else:
+        assert request.session
+        assert request.session.session_key is 'totallyfake'
